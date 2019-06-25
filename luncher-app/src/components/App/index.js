7631fe362ba6d6d1299ac49febddde395feffcb3
Noble Obioma <nobioma1@gmail.com>
2019-06-25 19:45:17 +0100

    Create url consts

--- luncher-app/src/components/App/index.js
+++ luncher-app/src/components/App/index.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import LogIn from '../LogIn';
 import SignUp from '../SignUp';
 import Header from '../Header';
 import { MainContainer } from './appStyle';
 import Home from '../Home';
 import AddSchoolForm from '../Addschool/index';
+import routes from '../../consts/urls';
+import { loggedIn } from '../../actions/login';
+import { getToken } from '../../helpers/localStorage';
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <MainContainer>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={LogIn} />
-          <Route path="/register" component={SignUp} />
-          <Route path="/addSchool" component={AddSchoolForm} />
-        </MainContainer>
-      </div>
-    </Router>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    const token = getToken();
+    if (token) {
+      this.props.loggedIn();
+    }
+  }
 
-export default App;
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <MainContainer>
+            <Route exact path={routes.home} component={Home} />
+            <Route path={routes.login} component={LogIn} />
+            <Route path={routes.signup} component={SignUp} />
+            <Route path={routes.addSchool} component={AddSchoolForm} />
+          </MainContainer>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { loggedIn },
+)(App);
