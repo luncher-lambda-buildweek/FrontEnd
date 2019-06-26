60c0827def357b99d0301ed20c4dba1a48632686
Noble Obioma <nobioma1@gmail.com>
2019-06-26 08:43:43 +0100

    Conditionally render, home content based on role

--- luncher-app/src/components/Home/index.js
+++ luncher-app/src/components/Home/index.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Schools from '../Schools';
 import { Title, Alert } from '../../globals/styles';
 import { HomeContainer } from './homeStyle';
 import routes from '../../consts/urls';
+import roles from '../../consts/roles';
 
 const Home = props => {
   return (
-    <div>
-      <HomeContainer>
-        <Title>Your School</Title>
-        <Alert>
-          You have not added any school{' '}
-          <Link to={routes.addSchool}>Add School</Link>
-        </Alert>
-      </HomeContainer>
+    <HomeContainer>
+      {props.role === roles.admin && (
+        <React.Fragment>
+          <Title>Your School</Title>
+          <Alert>
+            You have not added any school{' '}
+            <Link to={routes.addSchool}>Add School</Link>
+          </Alert>
+        </React.Fragment>
+      )}
       <Schools />
-    </div>
+    </HomeContainer>
   );
 };
 
-export default Home;
+const mapStateToProps = ({ loginReducer }) => {
+  return {
+    role: loginReducer.role,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {},
+)(Home);
