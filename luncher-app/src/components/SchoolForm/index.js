fa3edf5f4a1affdc86b8b98cead6c000cbd4643f
Noble Obioma <nobioma1@gmail.com>
2019-06-27 13:02:37 +0100

    Refactor SchoolForm to upload image from local, add loader, and Add Regex to only take numbers in number fields

--- luncher-app/src/components/SchoolForm/index.js
+++ luncher-app/src/components/SchoolForm/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Form, Button, Input, Title } from '../../globals/styles';
+import { Pulse, Pop } from 'react-preloading-component';
+import { Form, Button, Input, Title, Alert } from '../../globals/styles';
 import { createSchool, getSchool, updateSchool } from '../../actions/schools';
 import routes from '../../consts/urls';
+import uploadImage from '../../helpers/cloudinary';
+import { AvatarContainer } from '../Donate/donateStyle';
 
 class SchoolForm extends React.Component {
   state = {
@@ -13,9 +16,10 @@ class SchoolForm extends React.Component {
       email: '',
       phoneNumber: '',
       fundsNeeded: '',
-      currentFunds: '',
+      currentFunds: 0,
       schoolImg: '',
     },
+    uploadingImage: false,
     error: '',
     isEdit: false,
     id: null,
@@ -45,8 +49,7 @@ class SchoolForm extends React.Component {
       this.state.newSchool.location.trim() &&
       this.state.newSchool.email.trim() &&
       this.state.newSchool.phoneNumber &&
-      this.state.newSchool.fundsNeeded &&
-      this.state.newSchool.currentFunds;
+      this.state.newSchool.fundsNeeded;
 
     return validate;
   };
@@ -60,26 +63,59 @@ class SchoolForm extends React.Component {
     });
   };
 
+  onNumberChange = event => {
+    const name = event.target.name;
+    const number = event.target.value;
+    if (!number || number.match(/^\d{1,}(\.\d{0,2})?$/)) {
+      this.setState({
+        newSchool: {
+          ...this.state.newSchool,
+          [name]: number,
+        },
+      });
+    }
+  };
+
+  handleImageUpload = e => {
+    this.setState({ uploadingImage: true });
+    const file = e.target.files[0];
+    uploadImage(file).then(res => {
+      if (res) {
+        this.setState(prevState => ({
+          ...prevState,
+          newSchool: { ...prevState.newSchool, schoolImg: res },
+        }));
+        this.setState({ uploadingImage: false });
+      }
+    });
+  };
+
   submitHandler = e => {
     e.preventDefault();
     const { newSchool, id, isEdit } = this.state;
     const notEmpty = this.validateInput();
     if (notEmpty) {
       !isEdit
-        ? this.props.createSchool(newSchool).then(res => this.goHome(res))
-        : this.props.updateSchool(newSchool, id).then(res => this.goHome(res));
+        ? this.props
+            .createSchool({ ...newSchool })
+            .then(res => this.goHome(res))
+        : this.props
+            .updateSchool({ ...newSchool }, id)
+            .then(res => this.goHome(res));
     } else {
       this.setState({ error: 'All Fields are Required' });
     }
   };
 
   render() {
-    const { isEdit } = this.state;
+    const { isEdit, newSchool, uploadingImage } = this.state;
+    const buttonText = isEdit ? 'Save Changes' : 'Add School';
     return (
       <div>
         <div>
           <Title>{isEdit ? 'Update' : 'Create'} your school's profile</Title>
           <Form onSubmit={this.submitHandler}>
+            {this.props.error && <Alert>{this.props.error}</Alert>}
             <Input
               name="schoolName"
               placeholder="School Name"
@@ -103,37 +139,32 @@ class SchoolForm extends React.Component {
               value={this.state.newSchool.email}
             />
             <Input
-              type="number"
               name="phoneNumber"
               placeholder="Phone Number"
               required
-              onChange={this.handleChanges}
+              onChange={this.onNumberChange}
               value={this.state.newSchool.phoneNumber}
             />
             <Input
-              type="number"
               name="fundsNeeded"
               placeholder="Funds needed ex. (1000.00)"
               required
-              onChange={this.handleChanges}
+              onChange={this.onNumberChange}
               value={this.state.newSchool.fundsNeeded}
             />
-            <Input
-              type="number"
-              name="currentFunds"
-              placeholder="Current funds ex. (10.00)"
-              required
-              onChange={this.handleChanges}
-              value={this.state.newSchool.currentFunds}
-            />
-            <Input
-              type="string"
-              name="schoolImg"
-              placeholder="Image URL"
-              onChange={this.handleChanges}
-              value={this.state.newSchool.schoolImg}
-            />
-            <Button>{isEdit ? 'Save Changes' : 'Add School'}</Button>
+            {uploadingImage ? (
+              <Pulse />
+            ) : (
+              newSchool.schoolImg && (
+                <AvatarContainer>
+                  <img src={newSchool.schoolImg} alt="uploaded avatar" />
+                </AvatarContainer>
+              )
+            )}
+            <Input type="file" onChange={this.handleImageUpload} />
+            <Button disabled={uploadingImage}>
+              {this.props.isSubmitting ? <Pop /> : buttonText}
+            </Button>
           </Form>
         </div>
       </div>
@@ -145,12 +176,14 @@ const mapStateToProps = ({ schoolReducer }) => {
   return {
     error: schoolReducer.error,
     school: schoolReducer.school,
+    isSubmitting: schoolReducer.isLoading,
   };
 };
 
 SchoolForm.propTypes = {
   error: PropTypes.string,
   createSchool: PropTypes.func,
+  isSubmitting: PropTypes.bool,
 };
 
 export default connect(
